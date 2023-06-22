7ce83a80e6740b047e0c669b65038e81055d6c38
arun-mericaninc <arun@mericaninc.com>
2023-06-23 01:19:56 +0530

    test1

--- app/create-post/page.jsx
+++ app/create-post/page.jsx
@@ -161,16 +161,16 @@ rounded-md text-white"
 
 export default CreatePost;
 
-export const getServerSideProps = async (context) => {
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      redirect: {
-        destination: "./profile",
-      },
-    };
-  }
-  return {
-    props: { session },
-  };
-};
+// export const getServerSideProps = async (context) => {
+//   const session = await getSession(context);
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "./profile",
+//       },
+//     };
+//   }
+//   return {
+//     props: { session },
+//   };
+// };
